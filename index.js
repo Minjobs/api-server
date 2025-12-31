d7f13f9560510f4bdad69c2de6aa8c01c6ea676c
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-01 01:14:08 +0900

    commit

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 require('dotenv').config();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('DB 연결 실패:', err);
+    return;
+  }
+  console.log('MySQL 연결 성공 ✅');
+});
+
+require('dotenv').config();
 
 const express = require('express');
 const app = express();

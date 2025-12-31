60b4f1ddd908aadba6c31d8475a370f017576f1a
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-01 01:10:44 +0900

    commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-// 기본 API
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('서버 정상 작동 중입니다');
+  res.send('API 서버 실행 중 🚀');
 });
 
-// 서버 실행
 app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중입니다.');
+  console.log('서버가 3000번 포트에서 실행 중');
 });

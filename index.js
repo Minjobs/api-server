44031e98bf65835320806f24582670c9050ba5ab
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-01 00:37:51 +0900

    second commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 
 // 기본 API
 app.get('/', (req, res) => {
-  res.send('서버 정상 작동 중 🚀');
+  res.send('서버 정상 작동 중입니다');
 });
 
 // 서버 실행
 app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중');
+  console.log('서버가 3000번 포트에서 실행 중입니다.');
 });

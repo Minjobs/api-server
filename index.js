9742ecbc7f667f335c11be150e52a2d810b79e02
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-04 18:14:26 +0900

    update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const mysql = require('mysql2/promise');
 // DB 연결 설정
 const db = mysql.createPool({
     host: 'localhost',
-    user: 'root',
+    user: 'api_user',
     password: 'password123',
     database: 'client_db'
 });

f6c159b94f3e9889fddfc0dfcc2efc4f47fa5b06
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-17 19:21:58 +0900

    update public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
-import { HomePageView } from './pages/HomePage/HomePageView.js';
+import { HomePageView } from './js/pages/homepage_view.js';
 
 // HomePageView를 루트로 앱 시작
 const app = createApp(HomePageView);

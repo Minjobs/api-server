d239d6df81ef99a311404645940b6303adc56da4
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-25 14:35:26 +0900

    update server/routes/viewRoutes.js

--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { verifyToken } from '../middlewares/authMiddleware.js'; // 미들웨어 불러오기
+
 const router = express.Router();
 const __dirname = path.resolve();
 
-// 각 주소에 맞게 HTML 파일을 보내줍니다.
+// 🔓 누구나 접근 가능
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client/views/index.html')));
 router.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'client/views/login.html')));
-router.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'client/views/home.html')));
-router.get('/profile', (req, res) => res.sendFile(path.join(__dirname, 'client/views/profile.html')));
+
+// 🔐 로그인한 사람만 접근 가능 (verifyToken 추가)
+router.get('/home', verifyToken, (req, res) => res.sendFile(path.join(__dirname, 'client/views/home.html')));
+router.get('/profile', verifyToken, (req, res) => res.sendFile(path.join(__dirname, 'client/views/profile.html')));
 
 export default router;

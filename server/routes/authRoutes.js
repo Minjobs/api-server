f02045b5e33fe471539ece21748bbeccd050c553
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-25 16:49:30 +0900

    update server/routes/authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get('/line', redirectToLine);     // 사용자가 로그인 버튼 누르면 가는 곳
 router.get('/callback', handleCallback); // 라인이 인증 후 돌아오는 곳
+router.get('/logout', logout);
+
 
 export default router;

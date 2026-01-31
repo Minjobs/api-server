2ae9aa399279f6a7f76ac6e60693fe26662bbc5c
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-01-31 19:32:48 +0900

    update server/routes/viewRoutes.js

--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -16,6 +16,8 @@ router.get('/profile', verifyToken, (req, res) => res.sendFile(path.join(__dirna
 router.get('/obanggi', verifyToken, (req, res) => res.sendFile(path.join(__dirname, 'client/views/obanggi.html')));
 //사주 입력 페이지
 router.get('/saju', verifyToken, (req, res) => res.sendFile(path.join(__dirname, 'client/views/saju.html')));
+//상점 페이지
+router.get('/shop', verifyToken, (req, res) => res.sendFile(path.join(__dirname, 'client/views/shop.html')));
 // 성격, 재물, 연애 모두 같은 input-form.html을 보여줍니다.
 router.get(['/personality', '/wealth', '/romance'], (req, res) => {
     res.sendFile(path.join(__dirname, 'client/views/input-form.html'));

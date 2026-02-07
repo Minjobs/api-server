6b15195a53bb79ceb01be232afd63c9dd07a6cfd
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-02-07 13:02:27 +0900

    update server/routes/loveRoutes.js

--- server/routes/loveRoutes.js
+++ server/routes/loveRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import * as loveController from '../controllers/loveController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
+import { verifyApiKey } from '../middlewares/apiKeyMiddleware.js';
 
 const router = express.Router();
+
+router.use(verifyApiKey);
+router.use(verifyToken);
 
 // [POST] /api/love/analyze
 router.post('/analyze', loveController.analyzeLove);

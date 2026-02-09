3c24a977d8c16efddd24e28376820b1ad19c80a4
Minjobs <101300199+Minjobs@users.noreply.github.com>
2026-02-09 18:33:35 +0900

    update server/routes/gachaRoutes.js

--- server/routes/gachaRoutes.js
+++ server/routes/gachaRoutes.js
@@ -6,9 +6,8 @@ import { verifyApiKey } from '../middlewares/apiKeyMiddleware.js';
 const router = express.Router();
 
 router.use(verifyApiKey);
-router.use(verifyToken);
 
-router.post('/analyze', gachaController.analyzeGacha);
+router.post('/analyze', verifyToken,gachaController.analyzeGacha);
 router.get('/result/:id', gachaController.getGachaResult);
 
 export default router;
